95b51dc8c70287d7f90c61cf224a50a8fccd34b6
Jhazmin Quino <quisajhaz@gmail.com>
2024-01-16 09:16:18 -0400

    Contador

--- nodejs/mi-primera-app/src/App.js
+++ nodejs/mi-primera-app/src/App.js
@@ -1,36 +1,55 @@
 import './App.css';
-import Evento from './componentes/Evento.js'
-import EventoB from './componentes/EventoB.js'
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import { useState } from 'react';
 
 function App() {
+  const [nroClicks, setNumClicks] = useState(0);
+  const [show, setShow] = useState(true);
+  const click = () => {
+    setNumClicks(nroClicks + 1);
+    console.log("click");
+  }
+  const clickMas3 = () => {
+    setNumClicks(nroClicks + 3);
+
+  }
+  const clickMenos3 = () => {
+    setNumClicks(nroClicks - 3);
+
+  }
+  const clickMenos = () => {
+    setNumClicks(nroClicks - 1);
+
+  }
+  const reiniciar = () => {
+    setNumClicks(0);
+    console.log('reiniciar');
+  }
+  const mostrar = () => {
+    setShow(!show);
+  }
   return (
     <div className="App">
-      <Evento />
-      <EventoB title="Py Pizza 03 - PyPuzzle"
-        image="evento1"
-        dateLiteral="09 de Diciembre de 2023 - 13:30 GMT-4"
-        placeLiteral="Mr Pizza, 20 de Octubre Frente a la Plaza Abaroa, La Paz, Bolivia"
-        description="
-        ¡Hola Pythonista!
-        Participa en este Evento Presencial de la comunidad Python La Paz
-        ¿Cuánto conoces de Python?
-        Aprende a trabajar en equipo resolviendo increíbles acertijo
-        ¡Anímate a ser parte de esta actividad!
-        "
-      />
-      <EventoB title="MeetUp 11 - 3 Pitfalls de la Ingeniería de datos (donde no meter la pata)"
-        image="evento2"
-        dateLiteral="25 de Noviembre de 2023"
-        placeLiteral="Online"
-        description="
-        ¡Hola Pythonista!
-        Acompáñanos al MeetUp mensual de la comunidad Python La Paz
+      <div className="contenedor-principal">
+        <Contador nroClicks={nroClicks} mostrar={show} />
+        <div className='contendedor-botones'>
+          <div className='contendedor-botones-numeros'>
+            <Boton texto="-3" esBotonClick={true}
+              funcionClick={clickMenos3} />
+            <Boton texto="+3" esBotonClick={true}
+              funcionClick={clickMas3} />
+            <Boton texto="-1" esBotonClick={true}
+              funcionClick={clickMenos} />
+            <Boton texto="+1" esBotonClick={true}
+              funcionClick={click} />
+          </div>
 
-        En este MeetUp tendremos la participación de Valery Calderon Briz, Ingeniera en Telecomunicaciones actualmente trabajando como Ingeniera de datos Senior en One.Five
-        Anteriormente Co-organizadora de Pyladies MX y Líder de la comunidad Python Guatemala
-        ha impartido conferencias y talleres en diversas PyCon en múltiples países.
-        "
-      />
+          <Boton texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
+          <Boton texto="Mostrar/Ocultar" esBotonClick={false} funcionClick={mostrar} />
+        </div>
+      </div>
     </div>
   );
 }
